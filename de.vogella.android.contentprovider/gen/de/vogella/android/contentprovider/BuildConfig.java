7b6508294ad3d2076022a5e19af92adf42a3fb8b
Lars Vogel <Lars.Vogel@gmail.com>
2012-03-28 11:13:00 +0200

    Added Android projects

---
+++ de.vogella.android.contentprovider/gen/de/vogella/android/contentprovider/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.vogella.android.contentprovider;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ de.vogella.android.sqlite.first/gen/de/vogella/android/sqlite/first/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.vogella.android.sqlite.first;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
